213dafc62656f843b44f568c5acda5d8c0bad25c
SpeedyPeder <pederaasvarheim@gmail.com>
2025-10-30 17:56:19 +0100

    jepp

--- Test kode/Burgers.jl
+++ Test kode/Burgers.jl
@@ -25,13 +25,21 @@ display(plt)
 savefig(plt, "burgers_snapshots.png")
 
 #------ Compare limiters ------
-BurgersSim.compare_limiters_zoom(50, 1, 0.5; CFL=0.6,
+BurgersSim.compare_limiters_zoom(100, 1, 0.5; CFL=0.6,
                       limiters=[:minmod, :mc, :superbee, :vanleer])
 
 # --- Animation (GIF) ---
 gifpath = joinpath(@__DIR__, "burgers.gif")
 _ = BurgersSim.animate_burgers(N, L, T; CFL=CFL, limiter=:mc, fps=30, path=gifpath)
 
-x,u_min = BurgersSim.burgers_muscl_godunov(N,L,T; limiter=:minmod)
-_,u_sup = BurgersSim.burgers_muscl_godunov(N,L,T; limiter=:superbee)
-println("max diff = ", maximum(abs.(u_min .- u_sup)))
+
+# --- Compare different FV methods and compare with analytical solution ---
+# N=50 grid to highlight smearing/oscillations
+N, L, CFL, T = 200, 1.0, 0.45, 0.5
+
+# One method at a time; numeric as dots, analytic as line
+BurgersSim.burgers_compare_at(N, L, T; method=:upwind,      show_analytic=true, savepath="upwind.png")
+BurgersSim.burgers_compare_at(N, L, T; method=:laxfriedrichs, show_analytic=true, savepath="lf.png")
+BurgersSim.burgers_compare_at(N, L, T; method=:laxwendroff, show_analytic=true, savepath="lw.png")
+BurgersSim.burgers_compare_at(N, L, T; method=:muscl, limiter=:minmod, show_analytic=true, savepath="muscl_minmod.png")
+BurgersSim.burgers_compare_at(N, L, T; method=:muscl, limiter=:mc,     show_analytic=true, savepath="muscl_mc.png")
